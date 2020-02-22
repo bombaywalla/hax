f97947661f49d148d542c56fdfa8a44e15ea0251
Dorab Patel <dorabpatel@gmail.com>
2020-02-22 10:23:37 -0800

    Moved the (mock) db code to its own namespace.

--- src/syntereen/hax.clj
+++ src/syntereen/hax.clj
@@ -1,4 +1,5 @@
 (ns syntereen.hax
+  "The Hax server."
   (:require [clojure.spec.alpha :as s]
             [cambium.core :as log]
             [cambium.mdc  :as mlog]
@@ -9,98 +10,8 @@
             [yada.yada :as yada]
             [syntereen.hax.specs :as hspecs]
             [syntereen.hax.system :as system]
+            [syntereen.hax.db :as db]
             ))
-
-;;; ---------------- Mock db ----------------
-
-(defonce mock-db
-  (atom {}))
-
-(defn user-valid?
-  [user]
-  (s/valid? :hspecs/user user))
-
-(defn sanitize-db-user
-  "Sanitize the db user before sending back in a response."
-  [db-user]
-  (dissoc db-user :password :password-hash))
-
-(defn reset-mock-db
-  "Reset the mock-db to be empty.
-  REPL use only."
-  []
-  (reset! mock-db {}))
-
-(defn get-user-key
-  "The value to be used as the key for a `user` in the db."
-  [user]
-  (:email user))
-
-(defn get-user
-  "Gets the user specified by`user-key`. Else `nil`."
-  [user-key]
-  (get @mock-db user-key))
-
-(defn add-user
-  "Add a user.
-  Returns the added user. Or throws ex-info."
-  [user]
-  (let [user-key (get-user-key user)
-        maybe-user (when user-key (get-user user-key))]
-    (cond (not (user-valid? user)) (throw (ex-info "Invalid user info provided"
-                                                   {:error-code "invalid-user-info" :user user}))
-          (not user-key) (throw (ex-info "Could not get the key from the specified user"
-                                         {:error-code "user-no-key" :user user}))
-          maybe-user (throw (ex-info "User already exists"
-                                     {:error-code "user-exists" :user user}))
-          :else (let [passwd (:password user)
-                      ;; TODO: Consider adding pepper to the hash, in addition to the salt.
-                      passwd-hash (hashers/derive passwd {:alg :pbkdf2+sha512})
-                      stored-user (assoc (dissoc user :password) :password-hash passwd-hash)]
-                  (swap! mock-db assoc user-key stored-user)
-                  stored-user))))
-
-(defn delete-user
-  "Removes the user specified by `user-key`.
-  Whether or not it existed.
-  Returns `nil`."
-  [user-key]
-  (swap! mock-db dissoc user-key)
-  nil)
-
-(defn update-user
-  "Update the user specified by `user-key`.
-  Assumes a valid `updated-user`.
-  Returns `nil` if user does not exist.
-  Else non-nil."
-  [user-key updated-user]
-  (let [maybe-user (get-user user-key)]
-    (when maybe-user
-      (swap! mock-db assoc user-key updated-user))))
-
-(defn all-user-keys
-  "Returns a vector of all the user keys."
-  []
-  (into [] (keys @mock-db)))
-
-(defn set-user-token
-  "Set the token for the user to `token`. Overwrites any existing.
-  Returns `nil` on error.
-  Else non-nil."
-  [user-key token]
-  (let [maybe-user (get-user user-key)]
-    (when (and maybe-user token)
-      (swap! mock-db assoc-in [user-key :token] token)
-      token)))
-
-(defn remove-user-token
-  "Removes any token associated with the `user-key`.
-  Returns `nil` on error.
-  Else non-nil."
-  [user-key]
-  (let [maybe-user (get-user user-key)]
-    (when maybe-user
-      (swap! mock-db update-in [user-key dissoc :token]))))
 
 ;;; ---------------- Main ----------------
 
@@ -147,9 +58,9 @@
     (log/debug "\n----------------------------------------------------------------")
     (log/debug "The register API was called.")
     (log/debug (str "context parameters: " (pr-str user)))
-    (try (let [new-user (add-user user)
+    (try (let [new-user (db/add-user user)
                _ (log/debug (str "new-user: " (pr-str new-user)))
-               body {:user (sanitize-db-user new-user)}]
+               body {:user (db/sanitize-db-user new-user)}]
            (log/debug (str "response body: " (pr-str body)))
            (assoc response :status 200 :body body))
          (catch Exception e
@@ -163,10 +74,10 @@
   ;; {:parameters {:body {:user {:email ... :password ...}}}}
   (let [user (:user (:body (:parameters ctx)))
         response (:response ctx)
-        user-key (get-user-key user)
-        db-user (get-user user-key)
+        user-key (db/get-user-key user)
+        db-user (db/get-user user-key)
         token (gen-token db-user)
-        body {:user (assoc (sanitize-db-user db-user) :token token)}]
+        body {:user (assoc (db/sanitize-db-user db-user) :token token)}]
     (log/debug "\n----------------------------------------------------------------")
     (log/debug "The login API was called.")
     (log/debug (str "context parameters: " (pr-str user)))
@@ -178,7 +89,7 @@
 
 (defn do-list-users
   [ctx]
-  (all-user-keys))
+  (db/all-user-keys))
 
 (def access-control
   {
---
+++ src/syntereen/hax/db.clj
@@ -0,0 +1,99 @@
+(ns syntereen.hax.db
+  "The database to store Hax data."
+  (:require [clojure.spec.alpha :as s]
+            [cambium.core :as log]
+            [cambium.mdc  :as mlog]
+            [integrant.core :as ig]
+            [buddy.hashers :as hashers]
+            [syntereen.hax.specs :as hspecs]
+            ))
+
+(defonce mock-db
+  (atom {}))
+
+(defn user-valid?
+  [user]
+  (s/valid? :hspecs/user user))
+
+(defn sanitize-db-user
+  "Sanitize the db user before sending back in a response."
+  [db-user]
+  (dissoc db-user :password :password-hash))
+
+(defn reset-mock-db
+  "Reset the mock-db to be empty.
+  REPL use only."
+  []
+  (reset! mock-db {}))
+
+(defn get-user-key
+  "The value to be used as the key for a `user` in the db."
+  [user]
+  (:email user))
+
+(defn get-user
+  "Gets the user specified by`user-key`. Else `nil`."
+  [user-key]
+  (get @mock-db user-key))
+
+(defn add-user
+  "Add a user.
+  Returns the added user. Or throws ex-info."
+  [user]
+  (let [user-key (get-user-key user)
+        maybe-user (when user-key (get-user user-key))]
+    (cond (not (user-valid? user)) (throw (ex-info "Invalid user info provided"
+                                                   {:error-code "invalid-user-info" :user user}))
+          (not user-key) (throw (ex-info "Could not get the key from the specified user"
+                                         {:error-code "user-no-key" :user user}))
+          maybe-user (throw (ex-info "User already exists"
+                                     {:error-code "user-exists" :user user}))
+          :else (let [passwd (:password user)
+                      ;; TODO: Consider adding pepper to the hash, in addition to the salt.
+                      passwd-hash (hashers/derive passwd {:alg :pbkdf2+sha512})
+                      stored-user (assoc (dissoc user :password) :password-hash passwd-hash)]
+                  (swap! mock-db assoc user-key stored-user)
+                  stored-user))))
+
+(defn delete-user
+  "Removes the user specified by `user-key`.
+  Whether or not it existed.
+  Returns `nil`."
+  [user-key]
+  (swap! mock-db dissoc user-key)
+  nil)
+
+(defn update-user
+  "Update the user specified by `user-key`.
+  Assumes a valid `updated-user`.
+  Returns `nil` if user does not exist.
+  Else non-nil."
+  [user-key updated-user]
+  (let [maybe-user (get-user user-key)]
+    (when maybe-user
+      (swap! mock-db assoc user-key updated-user))))
+
+(defn all-user-keys
+  "Returns a vector of all the user keys."
+  []
+  (into [] (keys @mock-db)))
+
+(defn set-user-token
+  "Set the token for the user to `token`. Overwrites any existing.
+  Returns `nil` on error.
+  Else non-nil."
+  [user-key token]
+  (let [maybe-user (get-user user-key)]
+    (when (and maybe-user token)
+      (swap! mock-db assoc-in [user-key :token] token)
+      token)))
+
+(defn remove-user-token
+  "Removes any token associated with the `user-key`.
+  Returns `nil` on error.
+  Else non-nil."
+  [user-key]
+  (let [maybe-user (get-user user-key)]
+    (when maybe-user
+      (swap! mock-db update-in [user-key dissoc :token]))))
+
